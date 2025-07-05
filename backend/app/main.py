68ed8fedf0a08291450bd7297dccd7d82bb837d2
shashwat-pd <shashwatp07@gmail.com>
2025-07-05 21:54:37 +0545

    CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import (
     inventory,
     stores,
@@ -21,6 +22,16 @@ import models.store_department
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Inventory Management & Forecasting API")
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     stores.router,
